7678c6f89eb7e0c509db61e9b41f56e54f31cb17
matthew.miao <280536928@qq.com>
2021-10-22 15:21:16 +0800

    change frame

---
+++ game/src/main/java/Test.java
@@ -0,0 +1,17 @@
+import lifegame.ParentFrame;
+
+/**
+ * @author
+ * @date 2020/4/20
+ */
+public class Test {
+  public static void main(String[] args) throws InterruptedException {
+    // 生命游戏
+    ParentFrame de = new ParentFrame();
+    de.nextGeneration();
+
+    //        //谢尔宾斯基三角形
+    //        StandardPointSierpinskiFrame s = new StandardPointSierpinskiFrame();
+    //        s.run();
+  }
+}
